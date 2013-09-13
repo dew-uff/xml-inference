f3abfbef2549bc33f302132010dba824546f1f20
fabio.santos <fabiogsrj.2007@gmail.com>
2013-09-13 10:45:21 -0300

    Adicionando replace para substituir aspas simples por crase no conteúdo dos elementos.

--- src/br/ufrj/ppgi/parser/DefaultHandleSAX.java
+++ src/br/ufrj/ppgi/parser/DefaultHandleSAX.java
@@ -233,19 +233,19 @@ public class DefaultHandleSAX extends DefaultHandler {
     		if( ElementoXML.TipoElemento.FILHO != filhos.get(i).getTipo() || ElementoXML.TipoElemento.ATRIBUTO != filhos.get(i).getTipo()){
 	    		if ( bElementoMisto && filhos.get(i).getTipo() == ElementoXML.TipoElemento.TEXTO){
 	    			strConteudo += "xml/mixedElement";
-	    			strConteudo += "(" + filhos.get(i).getPai().getId() + ", " + filhos.get(i).getId() + ", '" + filhos.get(i).getConteudo() + "').\n";
+	    			strConteudo += "(" + filhos.get(i).getPai().getId() + ", " + filhos.get(i).getId() + ", '" + filhos.get(i).getConteudo().replace("'", "`") + "').\n";
 	    		}
 	    		else{
 		    		if( filhos.get(i).getTipo() == ElementoXML.TipoElemento.TEXTO ){
 		    			if ( filhos.get(i).getPai().getTipo() == ElementoXML.TipoElemento.ATRIBUTO ){
 		    				strConteudo += filhos.get(i).getPai().getNome().toLowerCase();
-			    			strConteudo += "(" + filhos.get(i).getPai().getPai().getId() + ", " + filhos.get(i).getPai().getId() + ", '" + filhos.get(i).getConteudo() + "').\n";
+			    			strConteudo += "(" + filhos.get(i).getPai().getPai().getId() + ", " + filhos.get(i).getPai().getId() + ", '" + filhos.get(i).getConteudo().replace("'", "`") + "').\n";
 		    			}
 		    			else{
 		    				imprimirPai(filhos.get(i).getPai().getPai());
 		    				strConteudo += filhos.get(i).getPai().getNome().toLowerCase();
 			    			strConteudo += "(" + filhos.get(i).getPai().getPai().getId() + ", " + filhos.get(i).getPai().getId() + ", '" + 
-			    							filhos.get(i).getConteudo()  + "').\n";
+			    							filhos.get(i).getConteudo().replace("'", "`")  + "').\n";
 			    			
 		    			}
 		    		}
@@ -275,7 +275,7 @@ public class DefaultHandleSAX extends DefaultHandler {
         boolean d = false;  
       
         for ( int i = 0; i < c.length; i++ ){  
-            // verifica se o char não é um dígito  
+            // verifica se o char nï¿½o ï¿½ um dï¿½gito  
             if ( Character.isLetterOrDigit( c[ i ] ) ) {  
                 d = true;
             }  
